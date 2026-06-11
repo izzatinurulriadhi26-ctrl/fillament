030e18cce481922eac26b4da62221e1651caa765
Allysa Fatima R <izzatinurulriadhi26@gmail.com>
2026-06-11 14:47:46 +0700

    praktikum14

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PostController::class,
+    'index'])->name('filamentblog.home');
+    
